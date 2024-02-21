1bfab196970365bc708bb88eeea2834f39798c4c
Rafael Gutkowski <nounderline@libred.org>
2024-02-21 10:41:08 +0800

    Add my Emacs-y key-chords

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,11 @@
 
 vim.keymap.set("n", "<return>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
-vim.keymap.set("n", "[,a", "<Space>ww", { desc = "Other widnow" })
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("v", "x", '"_x')
+
+vim.keymap.del("n", ";")
+
+vim.keymap.set("n", ";q", "<Space>bd", { silent = true })
+
+vim.keymap.set("n", ";a", "<Space>ww", { silent = true })
