8e3c09793164ad522e8f8aabb3cf915d5cbfe239
Rafael Gutkowski <nounderline@libred.org>
2024-02-29 15:44:18 +0800

    Introduce Zen mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,5 @@ vim.keymap.set("n", ";a", "<Space>ww", { silent = true })
 vim.keymap.set("n", "<Space>gg", "<cmd>Neogit<cr>", { desc = "Open Neogit (root)" })
 
 vim.keymap.set("n", "<Space>gG", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open Neogit (cwd)" })
+
+vim.keymap.set("n", "<Space>uz", "<cmd>Zen<cr>", { desc = "Enter Zen" })
