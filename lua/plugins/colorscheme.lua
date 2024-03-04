f6de1fefd0916172f0dc3a8cc992b5f56da8a3cd
Rafael Gutkowski <nounderline@libred.org>
2024-03-04 13:15:56 +0800

    Use catpucino color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      -- flavour = "mocha",
+      flavour = "latte",
+      transparent_background = true,
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
