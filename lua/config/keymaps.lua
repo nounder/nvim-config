76bfa8d4c76e678196be800489d9b431f071729a
Rafael Gutkowski <nounderline@libred.org>
2024-03-04 13:15:04 +0800

    Apply borders to a floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local Util = require("lazyvim.util")
 
 vim.keymap.set("n", "<return>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
@@ -11,10 +9,14 @@ vim.keymap.del("n", ";")
 
 vim.keymap.set("n", ";q", "<Space>bd", { silent = true })
 
-vim.keymap.set("n", ";a", "<Space>ww", { silent = true })
+--vim.keymap.set("n", ";a", "<Space>ww", { silent = true })
 
 vim.keymap.set("n", "<Space>gg", "<cmd>Neogit<cr>", { desc = "Open Neogit (root)" })
 
 vim.keymap.set("n", "<Space>gG", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open Neogit (cwd)" })
 
 vim.keymap.set("n", "<Space>uz", "<cmd>Zen<cr>", { desc = "Enter Zen" })
+
+vim.keymap.set("n", "<C-/>", function()
+  Util.terminal(nil, { border = "rounded" })
+end, { desc = "Terminal (root dir )" })
