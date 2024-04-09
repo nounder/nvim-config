d35d527768e15fb386dcb7e890cf4ce4c0ad37ed
Rafael Gutkowski <nounderline@libred.org>
2024-04-09 16:32:25 +0800

    Don't auto-insert comment leader when using o normal cmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+
+  -- Don't auto-insert comment leader when using o normal cmd
+  -- From: https://stackoverflow.com/questions/62459817
+  command = "set formatoptions-=o",
+})
