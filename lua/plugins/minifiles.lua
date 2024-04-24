10f88879a8dcf91c06c0e4cd7bf30f8c518e2a8e
Rafael Gutkowski <nounderline@libred.org>
2024-04-25 02:10:56 +0800

    Use mini.files exclusively

---
+++ lua/plugins/minifiles.lua
@@ -0,0 +1,24 @@
+return {
+  "hasnovski/mini.files",
+  opts = {
+    options = {
+      use_as_default_explorer = true,
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+  },
+}
