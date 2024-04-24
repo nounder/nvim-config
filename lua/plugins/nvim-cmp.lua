310c4c67a84d5f5e2a9f714df5292a308dda58c9
Rafael Gutkowski <nounderline@libred.org>
2024-04-25 02:41:24 +0800

    Remove emoji cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,17 +1,12 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
 
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
 
       opts.preselect = cmp.PreselectMode.None
-
-      -- table.insert(opts.mapping, {
-      --   ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      -- })
 
       opts.mapping = cmp.mapping.preset.insert({
         ["<CR>"] = cmp.config.disable,
@@ -20,9 +15,6 @@ return {
         ["<C-j>"] = require("cmp").mapping.select_next_item(),
         ["<C-k>"] = require("cmp").mapping.select_prev_item(),
       })
-
-      -- opts.preselect = cmp.PreselectMode.None
-      table.insert(opts.sources, { name = "emoji" })
     end,
   },
 }
