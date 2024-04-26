3aa8e86be3b4fc165ce0bec2e45a997efbf25614
Rafael Gutkowski <nounderline@libred.org>
2024-04-26 18:02:16 +0800

    More power to f

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,14 @@
 local Util = require("lazyvim.util")
 
+vim.keymap.del("n", ";")
+vim.keymap.del("n", "f")
+
 vim.keymap.set("n", "<return>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+vim.keymap.set("n", "gl", "<c-w>w", { desc = "Other window" })
 
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("v", "x", '"_x')
-
-vim.keymap.del("n", ";")
-
-vim.keymap.del("n", "f")
 
 vim.keymap.set("n", ";q", "<Space>bd", { silent = true })
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,8 +8,15 @@ return {
         function()
           require("telescope.builtin").resume()
         end,
-        desc = "Find Plugin File",
       },
+
+      {
+        "fl",
+        function()
+          require("telescope.builtin").resume()
+        end,
+      },
+
       {
         "<leader><space>",
         LazyVim.telescope("files", { show_untracked = true }),
@@ -17,6 +24,41 @@ return {
       {
         "ff",
         LazyVim.telescope("files", { show_untracked = true }),
+      },
+
+      {
+        "fb",
+        LazyVim.telescope("buffers"),
+      },
+
+      {
+        "/",
+        LazyVim.telescope("current_buffer_fuzzy_find"),
+      },
+
+      {
+        "fg",
+        LazyVim.telescope("live_grep"),
+      },
+
+      {
+        "fo",
+        function()
+          require("telescope.builtin").lsp_document_symbols({
+            symbols = require("lazyvim.config").get_kind_filter(),
+          })
+        end,
+        desc = "Goto Symbol (Buffer)",
+      },
+
+      {
+        "fs",
+        function()
+          require("telescope.builtin").lsp_dynamic_workspace_symbols({
+            symbols = require("lazyvim.config").get_kind_filter(),
+          })
+        end,
+        desc = "Goto Symbol (Workspace)",
       },
     },
     -- change some options
@@ -28,6 +70,7 @@ return {
         winblend = 0,
         mappings = {
           i = {
+            ["<C-h>"] = "close",
             ["<C-j>"] = "move_selection_next",
             ["<C-k>"] = "move_selection_previous",
             ["<C-l>"] = "file_edit",
