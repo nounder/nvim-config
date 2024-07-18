bd1debdc40a111a5f955a19cb18ae8cc200631b2
Rafael Gutkowski <nounderline@libred.org>
2024-07-18 20:37:27 +0200

    Use Helix-like treesitter incr selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,16 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-k>",
+          node_incremental = "<C-k>",
+          scope_incremental = false,
+          node_decremental = "<C-j>",
+        },
+      },
+
       textobjects = {
         select = {
           enable = true,
