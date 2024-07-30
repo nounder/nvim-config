00b0a703d248836b079112fecd601535cbeb82c3
Rafael Gutkowski <nounderline@libred.org>
2024-07-30 14:38:41 +0200

    More cozy zen-mode

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -2,6 +2,13 @@ return {
   {
     "folke/zen-mode.nvim",
     opts = {
+      window = {
+        width = 80,
+
+        options = {
+          laststatus = 0,
+        },
+      },
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
@@ -9,7 +16,7 @@ return {
   },
   {
     "folke/twilight.nvim",
-    enabled = false,
+    enabled = true,
     opts = {
       dimming = {
         alpha = 0.6,
