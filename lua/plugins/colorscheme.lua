9667ea27678b3749ea936aee3516fdffbb2eb647
Rafael Gutkowski <nounderline@libred.org>
2024-07-30 15:19:30 +0200

    Back to catpucino theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,17 @@
-local theme_mode = os.getenv("THEME_MODE")
-
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      transparent_mode = true,
-      flavour = theme_mode == "light" and "latte" or "dark",
-    },
-  },
-
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
     opts = {
-      flavour = theme_mode == "light" and "latte" or "mocha",
       transparent_background = true,
     },
   },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        if theme_mode == "light" then
-          require("catppuccin").load()
-        else
-          require("gruvbox").load()
-        end
-      end,
+      colorscheme = "catppuccin",
     },
   },
 }
