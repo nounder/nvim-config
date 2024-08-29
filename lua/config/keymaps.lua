6b21e12ced8693fc55848387248596f2db46aab5
Rafael Gutkowski <nounderline@libred.org>
2024-08-29 19:56:41 +0200

    Support OS level copy-pasting and stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,3 +39,12 @@ vim.keymap.set(
 vim.keymap.set("n", "<C-/>", function()
   Util.terminal(nil, { border = "rounded" })
 end, { desc = "Terminal (root dir )" })
+
+-- for neovide
+vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
