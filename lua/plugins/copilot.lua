a1421d4de1812dcec651dc13e71feb42c9dbb8b3
Rafael Gutkowski <nounderline@libred.org>
2024-09-02 00:38:37 +0200

    Add Supermaven and Avante.nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,60 @@
 return {
   {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    opts = {
+      hints = { enabled = true },
+    },
+    build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to setup it properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      --disable_keymaps = true,
+      keymaps = {
+        accept_suggestion = "<C-;>",
+        accept_word = "<C-j>",
+      },
+    },
+    -- config = function()
+    --   require("supermaven-nvim").setup({})
+    -- end,
+  },
+  {
     "zbirenbaum/copilot.lua",
+    enabled = false,
     opts = {
       suggestion = {
         enabled = true,
