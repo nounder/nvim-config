ccbfd8412cfc5f693877d6a7ddf1dd80efe20095
Rafael Gutkowski <nounderline@libred.org>
2024-09-19 23:58:28 +0200

    Add gruvbox and tokyonight colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+    },
+  },
+
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    lazy = false,
+    init = function()
+      vim.g.gruvbox_material_transparent_background = 2
+    end,
+  },
+
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -12,7 +28,7 @@ return {
     "LazyVim/LazyVim",
     priority = 1000,
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox-material",
     },
   },
 }
